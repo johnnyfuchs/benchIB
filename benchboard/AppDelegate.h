ba2f00d19ccbf8e38a8d69fa4482a94425780377
johnnyfuchs <johnnyfuchs@gmail.com>
2014-11-22 12:18:32 -0800

    Initial Commit

---
+++ benchboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  benchboard
+//
+//  Created by Johnny Sparks on 11/22/14.
+//  Copyright (c) 2014 beergrammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ benchboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  benchboard
+//
+//  Created by Johnny Sparks on 11/22/14.
+//  Copyright (c) 2014 beergrammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
